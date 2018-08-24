bf065d8d41319af8d51fd26e9f57715ddb843299
dieimesc <tecsysteminformatica@gmail.com>
2018-08-24 11:23:16 -0300

    Update Shifter.cs

--- SubtitleTimeshift/Shifter.cs
+++ SubtitleTimeshift/Shifter.cs
@@ -5,22 +5,23 @@ using System.Threading.Tasks;
 
 namespace SubtitleTimeshift
 {
-    public class Shifter
+     public class Shifter
     {
         async static public Task Shift(Stream input, Stream output, TimeSpan timeSpan, Encoding encoding, int bufferSize = 1024, bool leaveOpen = false)
         {
             StreamReader sr = new StreamReader(input, encoding);
-            StreamWriter sw = new StreamWriter(output,encoding);
-
-            while(!sr.EndOfStream)
+            StreamWriter sw = new StreamWriter(output, encoding);
+            int count = 0;
+            while (!sr.EndOfStream)
             {
                 string line = sr.ReadLine();
 
+                if (int.TryParse(line.Substring(0, line.Length), out count))
+                {
+                    sw.WriteLine(count.ToString());
+                    sw.WriteLine(TimeSpan.FromMilliseconds(Convert.ToDouble(sr.ReadLine())) + timeSpan);
+                }
             }
-
-
-
-
         }
     }
 }
